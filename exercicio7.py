701d7e19f54f4bdd3637968d00f472fe3031ddb7
DarioDistaso <dariodistaso@homtail.com>
2021-06-29 22:19:31 -0300

    inserção do loop for

--- exercicio7.py
+++ exercicio7.py
@@ -1,5 +1,17 @@
-# Exercício 7. Altere o código a seguir, para o menor número de linhas possível, mantendo o mesmo resultado: print(" *") print(" * *") print(" * *") print(" * *") print("*** ***") print(" * *") print(" * *") print(" *****")
+# Exercício 7. Altere o código a seguir, para o menor número de linhas possível, mantendo o mesmo resultado: 
+# print("     *") 
+# print("    * *") 
+# print("   *   *") 
+# print("  *     *") 
+# print(" ***   ***") 
+# print("   *   *") 
+# print("   *   *") 
+# print("   *****")
 
 
-# Impressão do mesmo resultado mas usando uma linha de código
-print(" *", "\n * *" * 3,"\n*** ***","\n * *" * 2," \n *****") 
+# Impressão do mesmo resultado mas usando uma linha de código e com o mesmo desenho da seta
+
+lista = ["      *", "     *  *", "    *    *", "   *      *", "  ***    ***", "     *   *","     *   *", "     *****"]
+
+for i in lista:
+    print(i)
